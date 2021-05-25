22832282f8235a5ef6a4a0a85bc136098ad85076
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-05-25 13:58:03 -0400

    Update and rename SimpleInventoryTrackerInsertTag.php to AddUserFieldsTags.php

--- system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php
+++ system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php
@@ -2,9 +2,9 @@
 
 	namespace CustomTags;
 
-	use Asc\Model\SimpleInventoryTracker;
+	use Bcs\Model\AddUserFields;
 
-	class SimpleInventoryTrackerInsertTag
+	class AddUserFieldsTags
 	{
 		public function onReplaceTag (string $insertTag)
 		{
@@ -19,18 +19,13 @@
 			// lets make decisions based on the beginning of the tag
 			switch($arrTag[0]) {
 				// if the tag is what we want, {{simple_inventory::id}}, then lets go
-				case 'simple_inventory':
-					// take our id, $arrTag[1], and pull our data out and return it
-					$ourInfo = SimpleInventoryTracker::findOneBy('id', $arrTag[1]);
-					// for now, lets just return our ID to show we can get here
-					return $ourInfo->product_inventory;
+				case 'avatar':
+					return "SHOW AVATAR";
 				break;
-
-				// if we want to have other tags do other things they would go here
+				case 'bio':
+					return "SHOW BIO";
+				break;
 			}
-
-			// this is an example from Andy for how I can pull that data out
-			// <Model_Name>::findBy('<field_name>', 'lookup_data');
 
 			// something has gone horribly wrong, let the user know and hope for brighter lights ahead
 			return 'something_went_wrong';
