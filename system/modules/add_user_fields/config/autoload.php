ea6597d02f8a6a251ebbcbcfdcd2824ea113122d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-05-25 13:54:09 -0400

    Update autoload.php

--- system/modules/add_user_fields/config/autoload.php
+++ system/modules/add_user_fields/config/autoload.php
@@ -9,3 +9,10 @@
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
+
+
+/* Register Classes */
+ClassLoader::addClasses(array
+(
+	'CustomTags\AddUserFieldsTags' 		=> 'system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php'
+));
