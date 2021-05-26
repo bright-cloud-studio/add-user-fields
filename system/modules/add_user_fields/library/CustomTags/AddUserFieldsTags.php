e404a258417b455e6d3d3d5e4fa11b19b442d732
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-05-26 11:26:35 -0400

    Update AddUserFieldsTags.php

--- system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php
+++ system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php
@@ -23,6 +23,7 @@ class AddUserFieldsTags extends \System
 				
 				$imgSize = deserialize($objMember->user_image_size);
 				$imgMeta = deserialize($objMember->user_image_meta);
+				var_dump($imgMeta);
 				
 				if($objMember) {
 					$strImage = $objMember->user_image;
@@ -31,7 +32,7 @@ class AddUserFieldsTags extends \System
 					}
 					$objFile = \FilesModel::findByUuid($strImage);
 					$strPath = $objFile->path;
-					return "<img alt='".$imgMeta[0]."' title='".$imgMeta[1]."' caption='".$imgMeta[2]."' link='".$imgMeta[3]."' width='".$imgSize[0]."' height='".$imgSize[1]."' class='user_image' id='user_image id_" . $arrTag[1] . "' src='" . $strPath . "'>";
+					return "<img alt='".$imgMeta->alt."' title='".$imgMeta[1]."' caption='".$imgMeta[2]."' link='".$imgMeta[3]."' width='".$imgSize[0]."' height='".$imgSize[1]."' class='user_image' id='user_image id_" . $arrTag[1] . "' src='" . $strPath . "'>";
 				}
 				return "user_image - NO RESULT";
 			break;
