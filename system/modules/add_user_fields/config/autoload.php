30b357432b5d77e94a689b32fc71745236b2d4e0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-05-26 13:17:02 -0400

    Update autoload.php

--- system/modules/add_user_fields/config/autoload.php
+++ system/modules/add_user_fields/config/autoload.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Add User Fields - Adds custom fields to Contao Users
+ * Bright Cloud Studio's Add User Fields
  *
  * Copyright (C) 2021 Bright Cloud Studio
  *
- * @package    BrightCloudStudio/LamvinRepFinder
+ * @package    bright-cloud-studio/add-user-fields
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
- */
+**/
 
 
 /* Register Classes */
