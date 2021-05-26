6b55d679ec9363d381db56b16a2335d4bfdf0c31
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-05-26 10:37:50 -0400

    Update tl_user.php

--- system/modules/add_user_fields/dca/tl_user.php
+++ system/modules/add_user_fields/dca/tl_user.php
@@ -12,10 +12,19 @@
 
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_user']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_user']['palettes'][$k] = str_replace('email;', 'email;{add_user_fields_legend},user_image,user_bio;', $v);
+    $GLOBALS['TL_DCA']['tl_user']['palettes'][$k] = str_replace('email;', 'email;{add_user_fields_legend},user_image,user_image_size,user_bio;', $v);
 }
 
 /* Add fields to tl_user */
+
+$GLOBALS['TL_DCA']['tl_user']['fields']['user_bio'] = array
+(
+	'label'			=> &$GLOBALS['TL_LANG']['tl_user']['user_bio'],
+	'inputType'		=> 'textarea',
+	'eval'                	=> array('mandatory'=>true, 'rte'=>'tinyMCE','tl_class'=>'long'),
+	'sql'                   => "mediumtext NOT NULL default ''"
+);
+
 $GLOBALS['TL_DCA']['tl_user']['fields']['user_image'] = array
 (
 	'label'                   => &$GLOBALS['TL_LANG']['tl_user']['user_image'],
@@ -32,10 +41,13 @@ $GLOBALS['TL_DCA']['tl_user']['fields']['user_image'] = array
 	'sql'                    => ['type' => 'binary', 'length' => 16, 'notnull' => false, 'fixed' => true]
 );
 
-$GLOBALS['TL_DCA']['tl_user']['fields']['user_bio'] = array
+$GLOBALS['TL_DCA']['tl_user']['fields']['user_image_size'] = array
 (
-	'label'			=> &$GLOBALS['TL_LANG']['tl_user']['user_bio'],
-	'inputType'		=> 'textarea',
-	'eval'                	=> array('mandatory'=>true, 'rte'=>'tinyMCE','tl_class'=>'long'),
-	'sql'                   => "mediumtext NOT NULL default ''"
+	'label'                 => &$GLOBALS['TL_LANG']['tl_user']['user_image_size'],
+	'exclude'               => true,
+	'inputType'             => 'imageSize',
+	'options'               => \Contao\System::getImageSizes(),
+	'reference'             => &$GLOBALS['TL_LANG']['MSC'],
+	'eval'                  => ['size', 'rgxp'=>'natural', 'includeBlankOption'=>true, 'nospace'=>true, 'helpwizard'=>true, 'tl_class'=>'w50'],
+	'sql'                   => ['type' => 'string', 'length' => 64, 'default' => '']
 );
