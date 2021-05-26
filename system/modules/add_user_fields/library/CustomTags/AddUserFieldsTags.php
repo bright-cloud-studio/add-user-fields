9765cc8ba77ee4ddecc7764e3468c1fd49e9bff2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-05-26 13:18:24 -0400

    Update AddUserFieldsTags.php

--- system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php
+++ system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Bright Cloud Studio's Add User Fields
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/add-user-fields
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
 namespace CustomTags;
 use Contao\DataContainer;
 use Contao\ContentElement;
