72e81bacf83807f3e30b715750cfeba2f9ce5c3e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2021-06-04 10:55:30 -0400

    Update AddUserFieldsTags.php

--- system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php
+++ system/modules/add_user_fields/library/CustomTags/AddUserFieldsTags.php
@@ -76,7 +76,7 @@ class AddUserFieldsTags extends \System
 		return 'something_went_wrong';
 	}
 	
-	private function console_log($output, $with_script_tags = true) {
+	protected function console_log($output, $with_script_tags = true) {
 		$js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . ');';
 		if ($with_script_tags) {
 			$js_code = '<script>' . $js_code . '</script>';
