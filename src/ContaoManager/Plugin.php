2cfe25a2cf8f99f3598f9103159fbaabc1979df8
johnadapt <2372574+johnadapt@users.noreply.github.com>
2025-05-26 15:12:09 -0700

    fix2

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace BrightCloudStudio\AddUserFieldsBundle\ContaoManager;
 
+use BrightCloudStudio\AddUserFieldsBundle\AddUserFieldsBundle;
 use Contao\CoreBundle\ContaoCoreBundle;
-use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Plugin;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 
 class Plugin extends Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(\BrightCloudStudio\AddUserFieldsBundle::class)
+            BundleConfig::create(AddUserFieldsBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
